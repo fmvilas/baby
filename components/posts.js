51cee50f1403a84e9764f87e89f7023657586f12
anbreaker <antunez19@gmail.com>
2020-10-15 19:29:37 +0200

    segundo post

--- components/posts.js
+++ components/posts.js
@@ -1,9 +1,11 @@
-import HolaMundo from "./hola-mundo";
+import HolaMundo from './hola-mundo';
+import HolaPersonitas from './hola-personitas';
 
 export default function Posts() {
   return (
     <>
       <HolaMundo />
+      <HolaPersonitas />
     </>
-  )
+  );
 }
