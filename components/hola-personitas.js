c3a8fde7099851d722e1a0914d1c6c58987360df
anbreaker <antunez19@gmail.com>
2020-10-15 19:32:57 +0200

    Segundo post del blog, componente by anbreaker

---
+++ components/hola-personitas.js
@@ -0,0 +1,33 @@
+export default function HolaPersonitas({className = ''}) {
+  return (
+    <div className={`${className} mb-20`}>
+      <h2 className='font-blueberry text-2xl'>Hola con cariño de Antúnez 🤗</h2>
+      <div className='text-teal-600 mt-1 text-sm'>15 de Octubre de 2020</div>
+      <p className='text-xl mt-6'>
+        Hola, mamá, hola papá, me ha pedido Antúnez que escriba en mi blog para contaros
+        algunas cosas. También me dice que esta nervioso por verme llegar y que tratará de
+        ser una buena influencia (o al menos lo va a intentar 😇).
+      </p>
+      <p className='text-xl mt-6'>
+        El no sabía que ese Suizo, que en su día le ignoraba, (dabas vueltas con la cabeza
+        y así estás ahora pápa...) Y qué cuando le hizo caso fue por interés común, fue
+        una gran decisión para ambos.
+      </p>
+      <p className='text-xl mt-6'>
+        Javier me dice que la amistad que tiene con papá es de esas amistades que no se
+        buscan y que la vida te regala si la aceptas tal como son y logras superar tus
+        manías para crecer como persona.
+      </p>
+      <p className='text-xl mt-6'></p>
+      <p className='text-xl mt-6'>
+        Me pide que escriba este topicazo y yo solo lo hago por ser él, pero es cierto que
+        los amigos son la familia que se elige.
+      </p>
+      <p className='text-xl mt-6'>Con todo el amor y cariño de un amigo 🧡.</p>
+      <p className='text-xl mt-6'>
+        Pos Data: A Jéssica diz que me vai ensinar português, para que o pai e o Javier
+        não me ensinem parvoices e palavras feias.
+      </p>
+    </div>
+  );
+}
