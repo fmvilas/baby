5760bbe34d9671c31d08544436e1c4146e7864f8
fmvilas <fmvilas@gmail.com>
2020-10-19 18:56:08 +0200

    Mi captora es una falsaria

---
+++ components/mi-captora-es-una-falsaria.js
@@ -0,0 +1,36 @@
+export default function MiCaptoraEsUnaFalsaria({ className = '' }) {
+  return (
+    <div className={`${className} mb-20`}>
+      <h2 className="font-blueberry text-2xl">Mi captora es una falsaria</h2>
+      <div className="text-teal-600 mt-1 text-sm">19 de Octubre de 2020</div>
+      <p className="text-xl mt-6">
+        Son las 10.30 horas de la mañana y ya hemos vuelto al frenesí. Mi portadora, esa simpática mujer de sonrisa dulce -según mis informes-, no es lo que quiere parecer. Ni siquiera es lo que parece. Tras unas primeras semanas de crecimiento en fase de letargo, mis crecientes extremidades me permiten explorar los límites de mi prisión materna. Son ya demasiados meses en los que me gana la partida y permanezco en cautiverio, y cada día busco algo parecido a un teléfono, un fax, una paloma mensajera, un vaso de yogurt atado a una cuerda… algo que me permita enviar un mensaje de socorro al exterior.
+      </p>
+      <p className="text-xl mt-6">
+        Me explico: no es que sea insensible; sé que fuera ya está refrescando, que ahora a la gente le ha dado por cubrirse la boca hasta para bajar a por el pan… pero ansío la libertad. Tampoco niego que tiene su punto eso de estar flotando a treinta y tantos grados, que nadie haya venido a reclamarme el pago de mi estancia, o que no tengo que bajar la basura. Todo está automatizado aquí, la eficiencia prima. 
+      </p>
+      <p className="text-xl mt-6">
+        De hecho, podría pasar unos meses más sin ver la luz del sol, sin vislumbrar las caras que imagino asociadas a las voces que escucho. Sí, podría pasar sin ver la última temporada de La Casa de Papel, no vi la primera. Yo aún no existía. Y Úrsula Corberó está ya hasta en la sopa. El mes pasado salía en un mural de una marca de cereales que alguien instaló a dos o tres centímetros de mi cara. Ya está bien.
+      </p>
+      <p className="text-xl mt-6">
+        Bueno que me desvío del tema. Sólo quería que supieras que ando por aquí y que iré escribiendo de tanto en tanto.
+        Perdona que haya tardado tanto en hablar pero es que he tenido que aprender a programar para hacerme este blog.
+      </p>
+      <p className="text-xl mt-6">
+        Lo que no aguanto, decía, es vivir en una farsa. Mi portadora, que va de sensible, de algo cultureta, tiene un oscuro secreto, y cualquiera que acceda a sus listas de música podrá corroborarlo. Cada día, cuando nadie mira, cuando el otro autor intelectual de mi encierro no la ronda, ella se arrebata. Bata, bata, bata. Ella se alborota. Bota, bota, bota.
+      </p>
+      <p className="text-xl mt-6">
+        Entonces, mi burbuja, que habitualmente es un pacífico caldo, se convierte en un mar embravecido en el que no hay dulzura ni paz. Sólo perreo. De mis sensaciones se diría que no se conforma con perrear a la escoba, subirse a la encimera -ella, diva-, con usar la dichosa aguja de tejer como micro. Ella dice que está tejiendo, sí, pero en realidad no para de contonearse, de rozarse con los muebles, de sonreír a los espejos.
+      </p>
+      <p className="text-xl mt-6">
+        Ella, que criticó el reggaetón durante años. Ella, que iba de gruncia, de perroflauta y de antisistema por la vida. Os tiene a todos engañados. Quitaos las vendas de los ojos. ¿Fue o no fue ella la que echó de una tarima a un gogó de metro ochenta y cinco de estatura, musculado y en pie de guerra por este ataque? ¿Es o no ella la primera en buscar un disfraz cuando alguien plantea un guateque, toque o no en tiempo de pandemia? No habré nacido aún, pero me informo.
+      </p>
+      <p className="text-xl mt-6">
+        Con todo, lo peor no es que reniegue del reggaetón en público para después regodearse de la existencia de Daddy Yankee en privado.  Lo peor es que aún quedan algunas meses para que me enseñe a hacer twerking. Mami, estoy deseando que hagamos coreos conjuntas.
+      </p>
+      <p className="text-xl mt-6">
+        No habrá paz para los aburridos. Están avisados.
+      </p>
+    </div>
+  )
+}
--- components/posts.js
+++ components/posts.js
@@ -1,9 +1,11 @@
 import HolaMundo from "./hola-mundo";
 import HolaPersonitas from "./hola-personitas";
+import MiCaptoraEsUnaFalsaria from "./mi-captora-es-una-falsaria";
 
 export default function Posts() {
   return (
     <>
+      <MiCaptoraEsUnaFalsaria />
       <HolaPersonitas />
       <HolaMundo />
     </>
