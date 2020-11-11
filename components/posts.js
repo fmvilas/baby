cd22219e65ae9cf32d6c9d5d14f05ae1a2c1c6a8
fmvilas <fmvilas@gmail.com>
2020-11-11 20:11:31 +0100

    Añadir plan de entrenamiento

--- components/posts.js
+++ components/posts.js
@@ -1,10 +1,12 @@
 import HolaMundo from "./hola-mundo";
 import HolaPersonitas from "./hola-personitas";
 import MiCaptoraEsUnaFalsaria from "./mi-captora-es-una-falsaria";
+import PlanDeEntrenamiento from "./plan-de-entrenamiento";
 
 export default function Posts() {
   return (
     <>
+      <PlanDeEntrenamiento />
       <MiCaptoraEsUnaFalsaria />
       <HolaPersonitas />
       <HolaMundo />
