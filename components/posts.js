1f4e5e3f3a61a8f8c1ebeb1515d0264b58cc7acd
fmvilas <fmvilas@gmail.com>
2020-11-13 13:31:51 +0100

    Add PetitPois

--- components/posts.js
+++ components/posts.js
@@ -2,10 +2,12 @@ import HolaMundo from "./hola-mundo";
 import HolaPersonitas from "./hola-personitas";
 import MiCaptoraEsUnaFalsaria from "./mi-captora-es-una-falsaria";
 import PlanDeEntrenamiento from "./plan-de-entrenamiento";
+import PetitPois from "./petit-pois";
 
 export default function Posts() {
   return (
     <>
+      <PetitPois />
       <PlanDeEntrenamiento />
       <MiCaptoraEsUnaFalsaria />
       <HolaPersonitas />
