2a50333796fa5b3950ae3dc0407bb6f23d136edd
Fran Mendez <fmvilas@gmail.com>
2021-02-04 10:35:02 +0100

    Diez dias a solas con mama

---
+++ components/10-dias-a-solas-con-mama.js
@@ -0,0 +1,32 @@
+export default function DiezDiasASolasConMama({ className = '' }) {
+  return (
+    <div className={`${className} mb-20`}>
+      <h2 className="font-blueberry text-2xl">Diez días a solas con mamá</h2>
+      <div className="text-teal-600 mt-1 text-sm">4 de Febrero de 2021</div>
+      <p className="text-xl mt-6">
+        Hola a todas,
+      </p>
+      <p className="text-xl mt-6">
+        Llevo mucho tiempo sin pasar por aquí pero es que estoy muy entretenida creciendo, haciendo ejercicios para estar fuerte, jugando con el cordón umbilical y emocionada con la cantidad de emociones que estoy viviendo aquí dentro.
+      </p>
+      <p className="text-xl mt-6">
+        Ya hemos vuelto a la normalidad y a nuestra rutina habitual, pero mamá se contagio de un virus que tiene a todo el mundo aterrorizado, el famoso Covid. Pero no lo pasamos muy mal, fue algo muy leve, estuvimos juntitas y solo nos afecto unos días. Aún así estuvimos muchos días en la habitación solas, tiempo en el que estuvimos disfrutando la una de la otra. Ella hacia ejercicios, me ponía música y bailábamos y yo le daba patadas fuertes para que supiera que estaba bien y que dentro del útero todo estaba en orden. Es verdad que en esos días echaba mucho de menos a papá. Oía su voz de vez en cuando y de lejos, cuando más lo escuchaba era cuando venia a ayudar a mamá con los pinchazos que se tenía que poner en la barriga. Pero el día que volví a escuchar su voz cerca y a sentir su mano sobre el vientre de mamá, me puso muy feliz y me moví mucho mucho para enseñarle todo lo que había crecido en esos días. Me encanta que me hable y jugar con él.
+      </p>
+      <p className="text-xl mt-6">
+        Ya nos queda poco para vernos, pero yo voy a seguir aquí creciendo y haciéndome fuerte. Tengo muchas ganas de conocer a Kalea y Rua, creo que nos llevaremos bien porque tienen pinta de ser un poco gamberras, porque os escucho reñirles mucho. Creo que haremos un buen equipo de juegos las 3 :)
+      </p>
+      <p className="text-xl mt-6">
+        Me gusta mucho este hogar y sé que dentro de poco voy a tener que salir de aquí, porque el espacio empieza a ser reducido, pero aguantaré aquí un par de meses más y las ganas que tengo de conoceos a todo: a la bisa Maxi, a la abuela Pili, la abuela Carmen y el abuelo Martín, a todos los Titis, que Tío Antonio me enseñe cosas de científicos, bailar con Titi Grunci, que Titi Barbaño me cuente las cosas más increíbles, que Titi María me cuide y me mime mucho mucho mucho y de jugar con el divertido de Titi Chema. También tengo ganas de conocer a los primos y las primas, ¡qué me han dicho que tengo unas primas en León! ¡Qué ganas de conocer a Lola y a Lluvia!
+      </p>
+      <p className="text-xl mt-6">
+        Pero sobre todo, tengo muchas ganas de ver a Mamá y a Papá y de poder sentir sus abrazos, sus caricias y sus besos. 
+      </p>
+      <p className="text-xl mt-6">
+        Me queda mucho por descubrir y estoy muy ansiosa de conocer el mundo que me espera ahí fuera. Os quiero mucho a todos ya por todo el cariño que me dais a la distancia y cómo cuidáis y alegráis a mamá.
+      </p>
+      <p className="text-xl mt-6">
+        Nos vemos pronto familia.
+      </p>
+    </div>
+  )
+}
--- components/posts.js
+++ components/posts.js
@@ -3,10 +3,12 @@ import HolaPersonitas from "./hola-personitas";
 import MiCaptoraEsUnaFalsaria from "./mi-captora-es-una-falsaria";
 import PlanDeEntrenamiento from "./plan-de-entrenamiento";
 import PetitPois from "./petit-pois";
+import DiezDiasASolasConMama from "./10-dias-a-solas-con-mama";
 
 export default function Posts() {
   return (
     <>
+      <DiezDiasASolasConMama />
       <PetitPois />
       <PlanDeEntrenamiento />
       <MiCaptoraEsUnaFalsaria />
