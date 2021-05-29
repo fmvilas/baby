23bfc57dee6c302c7424cfc96f25b5615cc96c34
Fran Mendez <fmvilas@gmail.com>
2021-05-30 00:20:57 +0200

    Add ya voy

--- components/posts.js
+++ components/posts.js
@@ -5,10 +5,12 @@ import PlanDeEntrenamiento from "./plan-de-entrenamiento";
 import PetitPois from "./petit-pois";
 import DiezDiasASolasConMama from "./10-dias-a-solas-con-mama";
 import ContratoDeRecomenderConLovely from "./contrato-de-recomender-con-lovely";
+import YaVoy from "./ya-voy";
 
 export default function Posts() {
   return (
     <>
+      <YaVoy />
       <ContratoDeRecomenderConLovely />
       <DiezDiasASolasConMama />
       <PetitPois />
